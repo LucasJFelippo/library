802f95c43c24e288baf982f2a7d7986879e905bf
lucasdefelippe <lucasjfelippo@gmail.com>
2018-04-22 22:24:26 -0300

    semi-final 9

--- sw.js
+++ sw.js
@@ -1,35 +1,22 @@
-//This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+//NEW
+//This is the "Offline copy of pages" wervice worker
+//Install stage sets up the index page (home page) in the cahche and opens a new cache
+self.addEventListener('install', function (event) {
+  var indexPage = new Request('index.html');
+  event.waitUntil(
+      fetch(indexPage).then(function (response) {
+          caches.open('pwabuilder-offline').then(function (cache) {
+              console.log('[PWA Builder] Cached index page during Install' + response.url);
+              return cache.addAll(['/library2/','/library2/index.html','/library2/friends.html','/library2/library.html','/library2/offline.html','/library2/profile.html','/library2/singin.html','/library2/store.html','/library2/css/friends.css','/library2/css/library.css','/library2/css/login.css','/library2/css/navbar.css','/library2/css/profile.css','/library2/css/store.css','/library2/js/login.js','/library2/js/navbar.js','/library2/js/singin.js','/library2/images/game-icon.png','/library2/images/icon.jpg','/library2/images/logo.png','/library2/images/manifest-icon.png']);
+          });
+      })
+  );
+});
 
-//Install stage sets up the offline page in the cahche and opens a new cache
-self.addEventListener('install', function(event) {
-  event.waitUntil(preLoad());
-});
-var preLoad = function(){
-  console.log('[PWA Builder] Install Event processing');
-  return caches.open('pwabuilder-offline').then(function(cache) {
-    console.log('[PWA Builder] Cached index and offline page during Install');
-    return cache.addAll(['/library2/','/library2/index.html','/library2/friends.html','/library2/library.html','/library2/offline.html','/library2/profile.html','/library2/singin.html','/library2/store.html','/library2/css/friends.css','/library2/css/library.css','/library2/css/login.css','/library2/css/navbar.css','/library2/css/profile.css','/library2/css/store.css','/library2/js/login.js','/library2/js/navbar.js','/library2/js/singin.js','/library2/images/game-icon.png','/library2/images/icon.jpg','/library2/images/logo.png','/library2/images/manifest-icon.png']);
-  });
-}
+
+//If any fetch fails, it will look for the request in the cache and serve it from there first
 self.addEventListener('fetch', function(event) {
-  console.log('The service worker is serving the asset.');
-  event.respondWith(checkResponse(event.request).catch(function() {
-    return returnFromCache(event.request)}
-  ));
-  event.waitUntil(addToCache(event.request));
-});
-var checkResponse = function(request){
-  return new Promise(function(fulfill, reject) {
-    fetch(request).then(function(response){
-      if(response.status !== 404) {
-        fulfill(response)
-      } else {
-        reject()
-      }
-    }, reject)
-  });
-};
-var addToCache = function(request){
+var updateCache = function(request){
   return caches.open('pwabuilder-offline').then(function (cache) {
     return fetch(request).then(function (response) {
       console.log('[PWA Builder] add page to offline'+response.url)
@@ -37,14 +24,22 @@ var addToCache = function(request){
     });
   });
 };
-var returnFromCache = function(request){
-  return caches.open('pwabuilder-offline').then(function (cache) {
-    return cache.match(request).then(function (matching) {
-     if(!matching || matching.status == 404) {
-       return cache.match('offline.html')
-     } else {
-       return matching
-     }
+
+event.waitUntil(updateCache(event.request));
+
+event.respondWith(
+  fetch(event.request).catch(function(error) {
+    console.log( '[PWA Builder] Network request Failed. Serving content from cache: ' + error );
+
+    //Check to see if you have it in the cache
+    //Return response
+    //If not in the cache, then return error page
+    return caches.open('pwabuilder-offline').then(function (cache) {
+      return cache.match(event.request).then(function (matching) {
+        var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
+        return report
+      });
     });
-  });
-};
+  })
+);
+})
